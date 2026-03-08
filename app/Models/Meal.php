09f1f484ffd49fcc3eb6decec3d8a62f0a7aabd9
Israe <israe.yab@gmail.com>
2026-03-08 12:09:56 +0000

    one to many users and meals relationship

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-//model is the base class 
+
 class Meal extends Model
 {
     use HasFactory; //gives the model the factory() method for generating test data 
     protected $fillable = ['user_id', 'name', 'description', 'category']; //$fillable is an array of attributes, protection feature in Laravel Eloquent
+
+    public function user () {
+        return $this->belongsTo(User::class); 
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Meal;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,10 +18,14 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
+    public function meals () {
+        return $this->hasMany(Meal::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
