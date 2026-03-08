b3945a99034385695b52c00eb967f8f027c2de54
Israe <israe.yab@gmail.com>
2026-03-08 11:50:59 +0000

    added models with fillable and HasFactory

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//model is the base class 
 class Meal extends Model
 {
-    //
+    use HasFactory; //gives the model the factory() method for generating test data 
+    protected $fillable = ['user_id', 'name', 'description', 'category']; //$fillable is an array of attributes, protection feature in Laravel Eloquent
 }
