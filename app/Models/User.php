b50590bf4ffeb1539eae57f5b7893cbc7cc2ccbd
Israe <israe.yab@gmail.com>
2026-03-09 11:40:27 +0000

    fix: restore Kernel.php, add custom Authenticate middleware, and update API auth handling

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Laravel\Sanctum\HasApiTokens;
 use App\Models\Meal;
 use App\Models\Goal;
 use App\Models\MealPlan;
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function(){
+
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/meals', [MealController::class, 'index']);
 Route::post('/meals', [MealController::class, 'store']);
 Route::get('/meals/{id}', [MealController::class, 'show']);
 Route::put('/meals/{id}', [MealController::class, 'update']);
 Route::delete('/meals/{id}', [MealController::class, 'destroy']);
+});
 
 
 //AUTH routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
