ecd733e74b550f45e754423def298c85c5bcd460
Israe <israe.yab@gmail.com>
2026-03-09 00:53:44 +0000

    feat: add AuthController and auth routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register (Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users', //email must be unique in users table
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password), //hash the password
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login (Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        if (!Auth::attempt(['email' => $request->email, 'password'=> $request->password])) {
+            return response()->json(['message' => 'Invalid login details'], 401); //401 unauthorized
+        }
+        //find user n generate token
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        //return token 
+        return response()->json([
+            
+            'user' => $user,
+            'token' => $token
+        ]);
+        
+    }
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
@@ -13,3 +13,9 @@ Route::post('/meals', [MealController::class, 'store']);
 Route::get('/meals/{id}', [MealController::class, 'show']);
 Route::put('/meals/{id}', [MealController::class, 'update']);
 Route::delete('/meals/{id}', [MealController::class, 'destroy']);
+
+
+//AUTH routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
