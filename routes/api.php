814af9b6b798025c68ffc3c6ea6c8c86de316aa8
Israe <israe.yab@gmail.com>
2026-03-14 23:37:46 +0000

    feat: add ingredient route n tested it

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
-
+use App\Http\Controllers\IngredientController;
 
 Route::middleware('auth:sanctum')->group(function(){
 
@@ -14,6 +14,13 @@ Route::post('/meals', [MealController::class, 'store']);
 Route::get('/meals/{id}', [MealController::class, 'show']);
 Route::put('/meals/{id}', [MealController::class, 'update']);
 Route::delete('/meals/{id}', [MealController::class, 'destroy']);
+
+//Ingredient routes
+Route::get('/ingredients', [IngredientController::class, 'index']);
+Route::post('/ingredients', [IngredientController::class, 'store']);
+Route::get('/ingredients/{id}', [IngredientController::class, 'show']);
+Route::put('/ingredients/{id}', [IngredientController::class, 'update']);
+Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']);
 });
 
 
